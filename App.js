4f12bd6f1f50343e5c61c9420959182cbb7bfdef
Wasfan <mwasfan3@gmail.com>
2024-11-26 15:21:50 +0530

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>Hello <Text style={styles.text2}>WSO2</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text1: {
+    fontSize: 28,
+  },
+  text2: {
+    color: 'orange',
   },
 });
